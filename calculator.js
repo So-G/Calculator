1f1858465853979aa731fe7bbf76a512cfa95372
Solene <solene.gerard@gmail.com>
2021-03-09 14:30:07 +0100

    added variable

--- calculator.js
+++ calculator.js
@@ -1,1 +1,8 @@
-console.log ("Hello World!");
+const firstValue = prompt ("What is the first value?");
+const operator = prompt ("what is  the operator?");
+const secondValue = prompt ("what is the second value?");
+
+
+console.log(firstValue);
+console.log(operator);
+console.log(secondValue);
